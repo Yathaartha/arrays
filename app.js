b79d8801b17ef8b2054d995f387799059190d44b
Yathaartha <maharjanyathaartha017@gmail.com>
2021-02-23 17:54:43 +0545

    creating arrays

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const numbers = [1, 2, 3];
+const numbers = [1, 2, 3];//best way to create array
 console.log(numbers);
 
-// const moreNumbers = Array(5, 2);
+// const moreNumbers = new Array("Hi!", "ASD!");//using a built-in constructor
+// // const moreNumbers = new Array(5);//creates 5 empty elements in the array
 // console.log(moreNumbers);
 
-// const yetMoreNumbers = Array.of(1, 2);
+// const moreNumbers = Array(5,2);//same as the one above ithout 'new'
+
+// const yetMoreNumbers = Array.of(1,20);
 // console.log(yetMoreNumbers);
 
-const listItems = document.querySelectorAll('li');
+const listItems = document.querySelectorAll("li");
 console.log(listItems);
 
+// const moreNumbers = Array.from("Hi");//passing multiple arguments give error. it splits the alphabets in a string if passed
 const arrayListItems = Array.from(listItems);
 console.log(arrayListItems);
+
+

bd133457f1eb0e52e87f78a736cee8799a5f189b
Yathaartha <maharjanyathaartha017@gmail.com>
2021-02-23 19:11:43 +0545

    map()

--- app.js
+++ app.js
@@ -78,17 +78,17 @@
 
 // console.log(maxIndex);
 
+// prices.forEach((price, index, prices) => {
+//   const priceObj = { index: index, taxAdjustedPrices: price * (1 + tax) };
+//   taxAdjustedPrices.push(priceObj);
+// });
+
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
-const taxAdjustedPrices = [];
 
-// for (const price of prices) {
-//   taxAdjustedPrices.push(price * (1 + tax));
-// }
-
-prices.forEach((price, index, prices) => {
+const taxAdjustedPrices = prices.map((price, index, prices) => {
   const priceObj = { index: index, taxAdjustedPrices: price * (1 + tax) };
-  taxAdjustedPrices.push(priceObj);
+  return priceObj;
 });
 
 console.log(taxAdjustedPrices);

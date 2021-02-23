ae4614a7b4c936eac5780e44ebcf0cfebcf9721c
Yathaartha <maharjanyathaartha017@gmail.com>
2021-02-23 18:31:38 +0545

    Slice()

--- app.js
+++ app.js
@@ -31,20 +31,27 @@
 // console.log(personalData[1]);//index starts from 0. length of array is maxIndex+1
 
 
-const hobbies = ["Sports", "Cooking"];
-hobbies.push("Reading");//adds to last position laso returns length
-hobbies.unshift("Coding");//adds element to the 1st position and returns it
-hobbies.pop();//removes the last element and returns it
-hobbies.shift();//removes the first element and returns it
-console.log(hobbies);
+// const hobbies = ["Sports", "Cooking"];
+// hobbies.push("Reading");//adds to last position laso returns length
+// hobbies.unshift("Coding");//adds element to the 1st position and returns it
+// hobbies.pop();//removes the last element and returns it
+// hobbies.shift();//removes the first element and returns it
+// console.log(hobbies);
 
 
-hobbies[1] = "Coding";//changes the value of the position
-hobbies[5] = "Reading";//if added to empty spot, new emty elements are created
-console.log(hobbies, hobbies[4]);
+// hobbies[1] = "Coding";//changes the value of the position
+// hobbies[5] = "Reading";//if added to empty spot, new emty elements are created
+// console.log(hobbies, hobbies[4]);
 
-hobbies.splice(1, 0, "Good Food");//insert element at position and delete elements
-console.log(hobbies);
+// hobbies.splice(1, 0, "Good Food");//insert element at position and delete elements
+// console.log(hobbies);
 
-const removedElements = hobbies.splice(0, 1);//deletes all items
-//negative values goes to the end
+// const removedElements = hobbies.splice(0, 1);//deletes all items
+// //negative values goes to the end
+
+const testResults = [1, 5.4, 1.5, 10.00, -5, 10];
+const storedResults = testResults.slice(0, 2);//slice also can be used to select ranges from an array
+
+testResults.push(5.91);//since arrays are reference based if u add elements to testResults, storedResults is also updated. though, slice makes it so that it does not update
+
+console.log(storedResults, testResults);

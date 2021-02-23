fc415f25948ed05fa39e198d4918ab05e1d6e1a3
Yathaartha <maharjanyathaartha017@gmail.com>
2021-02-23 19:16:51 +0545

    sorted

--- app.js
+++ app.js
@@ -91,4 +91,15 @@ const taxAdjustedPrices = prices.map((price, index, prices) => {
   return priceObj;
 });
 
-console.log(taxAdjustedPrices);
+// console.log(taxAdjustedPrices);
+
+const sortedPrices = prices.sort((a, b) => {
+  if (a > b) {
+    return 1;
+  } else if (a === b) {
+    return 0;
+  } else {
+    return -1;
+  }
+});
+console.log(sortedPrices.reverse()); //changing the sort() logic also reverses
